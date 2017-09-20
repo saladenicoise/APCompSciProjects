ac385b1b7a79e9520b1d12f536a5ef66e34753b6
Jules Petit <jules.laptop@gmail.com>
2017-09-20 11:15:48 -0400

    Stats

--- Lab2_JP/src/StatisitcsPackage.java
+++ Lab2_JP/src/StatisitcsPackage.java
@@ -1,6 +1,13 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * name: jules petit
+ * Version: 1.0
+ * Block: E
+ * Date of Creation: 9/20/17
+ */
+
 public class StatisitcsPackage {
 
 	public static void main(String[] args) {
@@ -17,24 +24,90 @@ public class StatisitcsPackage {
 		}
 		arraysize = input.nextInt();
 		int []numbers = new int[arraysize];
-		int linecounter = 0;
 		while(input.hasNextInt()) {
 			for(int i = 0; i < arraysize; i++) {
 				numbers[i] = input.nextInt();
 			}
-			
-			for(int i = 0; i < arraysize; i++) {
-				System.out.print(numbers[i] + " ");
-				linecounter++;
-				if(linecounter%10==0) {
-					System.out.print("\n");
+		}
+
+		System.out.println("Unsorted Array: ");
+		output(numbers, arraysize);
+		System.out.println("Sorted Array:");
+		sort(numbers, arraysize);
+		output(numbers, arraysize);
+		System.out.println("Mean of the array:");
+		System.out.println(mean(numbers, arraysize));
+		input.close();
+	}
+
+	/**
+	 * Outputs the array given an array and the size 
+	 * @param nums the array
+	 * @param size the size of the array
+	 */
+	private static void output(int[]nums, int size) {
+		int linecounter = 0;
+		for(int i = 0; i < size; i++) {
+			System.out.print(nums[i] + " ");
+			linecounter++;
+			if(linecounter%10==0) {
+				System.out.print("\n");
+			}
+		}
+	}
+
+	/**
+	 * A Bubble Sort Sorting System 
+	 * @param nums the array
+	 * @param size the size of the array
+	 */
+	private static void sort(int []nums, int size) {
+		boolean swapped = true; // Boolean variable to check when sort is done
+		int j = 0;
+		int temp;
+		while (swapped) { // While swapped = true repeat
+			swapped = false; // Set it to false so that when were done sorting it doesnt keep doing it
+			j++;
+			for (int i = 0; i < size - j; i++) { // For loop to actually swap the array
+				if (nums[i] > nums[i + 1]) {
+					temp = nums[i];
+					nums[i] = nums[i + 1];
+					nums[i + 1] = temp;
+					swapped = true; // Set it to true so we can continue sorting
 				}
 			}
 		}
-		input.close();
 	}
-	
-	public static void output(int arraysize) {
-		
+	//Math Functions
+	/**
+	 * Calculates the mean of our array
+	 * @param nums the array
+	 * @param size the size
+	 * @return the mean
+	 */
+	private static int mean(int []nums, int size) {
+		int add = 0;
+		for(int i = 0; i < size; i++) {
+			add = add + nums[i];
+		}
+		return add/size;
 	}
+
+	private static int median(int []nums, int size) {
+		int median = 0;
+		int size2 = 0;
+		double doublemedian = 0;
+		double []doublenums = new double[size];
+		for(int i = 0; i < size; i++) {
+			doublenums[i] = nums[i]; 
+		}
+		if(size%2 == 0) {
+			median = nums[size/2] + nums[(size/2) + 1];
+			median = median/2;
+		}else {
+			size2 = size/2 + 0.5;
+			doublemedian = doublenums[size2];
+		}
+	}
+
 }
