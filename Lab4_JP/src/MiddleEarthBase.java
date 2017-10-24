de42d03a046a814aa6002267d136f69f93c4cbc5
Jules Petit <jules.laptop@gmail.com>
2017-10-24 14:24:04 -0400

    Sort of a working multiply

--- Lab4_JP/src/MiddleEarthBase.java
+++ Lab4_JP/src/MiddleEarthBase.java
@@ -54,25 +54,53 @@ public class MiddleEarthBase {
 	public static int multiply(int num1, int num2, int base) {
 		//Finds the number of digits to see how many times to repeat
 		//For each digit of number 2 multiply by each digit of number 1, loops inside of loops.
-		int num1digit = 0, num2digit = 0, sum = 0, tens = 1, sumdigit = 0, digitcounter = 0, digithold = 0, carry = 0, result = 0, tens1 = 1;
+		int digitcounter2 = 0, num2digit = 0, tens = 1, sum = 0, sum2 = 0, digitcounter = 0, important = 1, tens1 = 1, sum3 = 0, sumhold = 0, digits = 0, digitcounter3 = 0, sumdigit = 0, tens2 = 1, carry = 0, result = 0, num1hold = num1, num2hold = num2, digits1 = 0, digits2= 0;
 		do {
+			num1hold = num1hold/10;
+			digits1++;
+			num2hold = num2hold/10;
+			digits2++;
+		}while(num2hold >= 1);
+		do {
+			tens1 = 1;
 			num2digit = (num2/tens) % 10;
-			digitcounter++;
-			sum = num2digit * num1 + sum;
-			tens = tens * 10;
+			digitcounter = 0;
+			sum2 = 0;
 			do {
-				sumdigit = (sum/tens1) % 10;
-				if(sumdigit > base) {
-					digithold = sumdigit%base;
+				sum = num2digit * ((num1/tens1) % 10);
+				if(sum > base) {
+					sum = sum%base;
 					carry = 1;
 				}
-				result = digithold + (carry * 10 * (int) Math.pow(10, digitcounter)) + result;
-				tens1 = tens1 * 10;
-			}while(sumdigit != 0);
-		}while(digitcounter != 4);
-		return sum;
-	}	
-
-
+				sum2 = sum2 + (sum * (int) Math.pow(10, digitcounter));
+				sum2 = sum2+carry;
+				tens1 *= 10;
+				digitcounter++;
+			}while(digitcounter <= digits2);
+			sum2 = sum2 * important;
+			sum3 = sum2 + sum3;
+			important *= 10;
+			tens *= 10;
+			digitcounter2++;
+		}while(digitcounter2 <= digits1);
+		sumhold = sum3;
+		do {
+			sumhold = sumhold/10;
+			digits++;
+		}while(sumhold >= 1);
+		do {
+			sumdigit = (sum3/tens2) % 10;
+			carry = 0;
+			if(sumdigit > base) {
+				sumdigit = sumdigit%base;
+				carry = 1;
+			}
+			result = result + (sumdigit * (int) Math.pow(10, digitcounter3)) + (carry * (int) Math.pow(10, digitcounter3));
+			tens2 *= 10;
+			digitcounter3++;
+		}while(digitcounter3 <= digits);
+		System.out.print("====");
+		return result;
+	}
 }
 
