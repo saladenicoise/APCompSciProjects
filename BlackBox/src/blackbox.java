c716397818fdfc581f114295f1fb67e1f917b0fb
Jules Petit <jules.laptop@gmail.com>
2017-12-01 11:16:38 -0500

    Implement tsome high quallity stuff

--- BlackBox/src/blackbox.java
+++ BlackBox/src/blackbox.java
@@ -5,23 +5,28 @@ public class blackbox {
 
 	public static void main(String[] args) {
 		char [][] board = setup(); // Row, Col
-		//int [][] board1 = new int [12][12]; // Row, Col
 		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome!");
-		System.out.println("Choose:");
-		System.out.println("(1) Shoot a Laser");
-		System.out.println("(2) Guess at mirror location");
-		System.out.println("(0) Quit the game");
-		System.out.print("\nEnter Choice: \n");
-		System.out.print("Please enter the coordinates: ");
-		int c1 = input.nextInt();
-		int c2 = input.nextInt();
+		int choice = 0;
+		do {
+			System.out.println("Welcome!");
+			display(board);
+			System.out.println("Choose:");
+			System.out.println("(1) Shoot a Laser");
+			System.out.println("(2) Guess at mirror location");
+			System.out.println("(0) Quit the game");
+			System.out.print("\nEnter Choice: ");
+			choice = input.nextInt();
+			if(choice == 1) {
+				//Shoot
+			}else if(choice == 2){ // Guess
+				guess(board);
+			}else {
+				//Nope
+			}
+		}while(choice != 0);
 		input.close();
-		display(board);
-		guess(board, c1, c2);
-		
 	}
-	
+
 	/* Functions:
 	 * Recursive shoot
 	 * Display √
@@ -30,7 +35,7 @@ public class blackbox {
 	 * Mirror Creator √
 	 * Setup Array √
 	 */
-	
+
 	public static char[][] setup() {
 		char [][]board = new char[10][10];
 		for(int a = 0; a < board.length; a++) {
@@ -40,20 +45,24 @@ public class blackbox {
 		}
 		Random random = new Random();
 		for(int c = 0; c <= 10; c++) {
-				board[(random.nextInt(10))][(random.nextInt(10))] = (char)(random.nextInt(2)+48);
-			}
-		return board;
+			board[(random.nextInt(10))][(random.nextInt(10))] = (char)(random.nextInt(2)+48);
 		}
-	
+		return board;
+	}
+
 	public static void display(char [][] board) {
 		System.out.println("   20212223242526272829"); // Top Numbers
 		for(int a = 19, b = 30; b < 40; a--, b++) {
 			System.out.print(a); // Left Numbers
 			for(char boards : board[b-30]) {
-				if(boards == 0) { // backward
+				if(boards == 0) { // backward placeholder
 					System.out.print(". ");
-				}else if(boards == 1) { //forward
+				}else if(boards == 1) { //forward placeholer
 					System.out.print(" .");
+				}else if(boards == 2) { // Backwards slash
+					System.out.print(" /");
+				}else if(boards == 3) { // Forwards slash
+					System.out.print("\\ ");
 				}else {
 					System.out.print(" " + boards);
 				}
@@ -62,34 +71,57 @@ public class blackbox {
 		}
 		System.out.println("   0 1 2 3 4 5 6 7 8 9"); // Bottom Numbers
 	}
-	
-	public static void guess(char[][] board, int num1, int num2) {
-		int cguess = 0, iguess = 0;
-		if(board[num1][num2] == '0' || board[num1][num2] == '1') { // Successful Guess
-			stats();
-		}else { //Unsucessfull
-			
+
+	public static boolean guess(char[][] board) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter which coordinate(left/right) to check for: ");
+		int a = in.nextInt();
+		if((9 < a) && (a < 20)) {
+			a = (-a + 19);
+		}else if((29 < a) && (a < 40)) {
+			a = a - 30;
+		}else {
+			System.out.println("Unfortunately that is not a correct input");
+			//correct = false; 
+		}
+		System.out.print("Please enter which coordinate(top/bottom) to check for: ");
+		int b = in.nextInt();
+		if((19 < b) && (b < 30)) {
+			b = b - 20;
+		}else if(b < 10) {
+			b = (b+1) - 1;
+		}else {
+			System.out.println("Unfortunately that is not a correct input");
+			//correct = false;
+		}
+		if(board[a][b] == '0') { // Backwards
+			board[a][b] = (char) 2;
+			System.out.println("That is correct! Mirror will now appear! MAGIC");
+		}else if(board[a][b] == '1') { // Forwards
+			board[a][b] = (char) 3;
+			System.out.println("That is correct! Mirror will now appear! MAGIC");
+		}else {
+			System.out.println("I'm sorry, that is incorrect!");
 		}
 	}
-	
-	public static void stats() {
-		System.out.print("Sucess!");
+
+	public static void stats(int condition) {
 	}
-	
-/*	public static int shoot(int num1, char[][]board) {
+
+	/*	public static int shoot(int num1, char[][]board) {
 		if(num1 < 20) { // Left and bottom input
 			if(num1 < 10) { // Bottom Input
-				
+
 			}else { // Left Input
-				
+
 			}
 		}else { // Right and top input
 			if(num1 < 30 ) { // Top Input
-				
+
 			}else { // Right Input
-				
+
 			}
 		}
 	}*/
-	
+
 }
