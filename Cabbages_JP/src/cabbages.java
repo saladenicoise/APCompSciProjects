105b66d83a8c8e9781b8b3c03bf4f3bd9b16218b
Jules Petit <jules.laptop@gmail.com>
2018-01-16 14:25:37 -0500

    LowerCase

--- Cabbages_JP/src/cabbages.java
+++ Cabbages_JP/src/cabbages.java
@@ -4,17 +4,36 @@ import java.io.*;
 
 public class cabbages {
 
-	public static Scanner in = null;
+	//public static Scanner in = null;
 	public static void main(String[] args) throws IOException {
-		String line = "";
+		String line = "", BiggestLine = "";
 		int b = 0, c = 0;
 		// TODO Auto-generated method stub
 		//Using buffered reader is much faster to read.
 		BufferedReader f = new BufferedReader(new FileReader("Cabbages.txt"));
-		in = new Scanner(f);
+		Scanner in = new Scanner(f);
 		while(in.hasNext()) {
 			b++;
 			line = in.next();
+			if(line.length() > BiggestLine.length()) {
+				BiggestLine = line;
+			}
+			System.out.println(b + " " + line);
+		}
+		System.out.println("The longest word in the text is: <" + BiggestLine + ">");
+		noPunctuation();
+		
+	}
+	
+	public static void noPunctuation() throws IOException{
+		BufferedReader f = new BufferedReader(new FileReader("Cabbages.txt"));
+		String line = "";
+		int b = 0;
+		Scanner in = new Scanner(f);
+		while(in.hasNext()) {
+			b++;
+			line = in.next().toLowerCase();
+			String stripped = line.replace(".", "");
 			System.out.println(b + " " + line);
 		}
 	}
