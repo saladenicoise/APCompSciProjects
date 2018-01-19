15a73ab4b26d4d1a8b8031ecb086023aa17f2d47
Jules Petit <jules.laptop@gmail.com>
2018-01-19 11:18:42 -0500

    Punctuation Removal

--- Cabbages_JP/src/cabbages.java
+++ Cabbages_JP/src/cabbages.java
@@ -1,10 +1,12 @@
 
 import java.util.*;
+
+import javax.naming.LimitExceededException;
+
 import java.io.*;
 
 public class cabbages {
 
-	//public static Scanner in = null;
 	public static void main(String[] args) throws IOException {
 		String line = "", BiggestLine = "";
 		int b = 0, c = 0;
@@ -27,15 +29,41 @@ public class cabbages {
 	
 	public static void noPunctuation() throws IOException{
 		BufferedReader f = new BufferedReader(new FileReader("Cabbages.txt"));
-		String line = "";
+		String line = "", duplicate = "", noPunctuation = "";
 		int b = 0;
 		Scanner in = new Scanner(f);
 		while(in.hasNext()) {
 			b++;
 			line = in.next().toLowerCase();
-			String stripped = line.replace(".", "");
-			System.out.println(b + " " + line);
+				if(line.indexOf('.') != -1) {
+					noPunctuation = line.replace(".", "");
+				}else if(line.indexOf(',') != -1) {
+					noPunctuation = line.replace(",", "");
+				}else if(line.indexOf(';') != -1) {
+					noPunctuation = line.replace(";", "");
+				}else if(line.indexOf(':') != -1) {
+					noPunctuation = line.replace(":", "");
+				}else if(line.indexOf('!') != -1) {
+					noPunctuation = line.replace("!", "");
+				}else if(line.indexOf('?') != -1) {
+					noPunctuation = line.replace("?", "");
+				}else if(line.indexOf("--") != -1) {
+					noPunctuation = line.replace("--", "");
+				}else if(line.indexOf('"') != -1) {
+					noPunctuation = line.replace("\"", "");
+				}else if(line.indexOf('\'') != -1) {
+					noPunctuation = line.replace("'", "");
+				}else if(line.indexOf('-') != -1) {
+					noPunctuation = line.replace("-", "");
+				}else {
+					noPunctuation = line;
+				}
+			System.out.println(b + " " + noPunctuation);
 		}
+	}
+	
+	public static String[] stringArray() throws IOException {
+		
 	}
 
 }
