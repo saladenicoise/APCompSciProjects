4105d2df5955d087a2988028512f00b19f393e6c
Jules Petit <jules.laptop@gmail.com>
2018-01-22 11:27:34 -0500

    Deletion of Dups

--- Cabbages_JP/src/cabbages.java
+++ Cabbages_JP/src/cabbages.java
@@ -23,47 +23,54 @@ public class cabbages {
 			System.out.println(b + " " + line);
 		}
 		System.out.println("The longest word in the text is: <" + BiggestLine + ">");
-		noPunctuation();
-		
+		output2();
+
 	}
-	
-	public static void noPunctuation() throws IOException{
+
+	public static void output2() throws IOException{
 		BufferedReader f = new BufferedReader(new FileReader("Cabbages.txt"));
 		String line = "", duplicate = "", noPunctuation = "";
 		int b = 0;
 		Scanner in = new Scanner(f);
-		while(in.hasNext()) {
+		String[] words = stringArray();
+		for(int a = 0; a < words.length; a++) {
 			b++;
-			line = in.next().toLowerCase();
-				if(line.indexOf('.') != -1) {
-					noPunctuation = line.replace(".", "");
-				}else if(line.indexOf(',') != -1) {
-					noPunctuation = line.replace(",", "");
-				}else if(line.indexOf(';') != -1) {
-					noPunctuation = line.replace(";", "");
-				}else if(line.indexOf(':') != -1) {
-					noPunctuation = line.replace(":", "");
-				}else if(line.indexOf('!') != -1) {
-					noPunctuation = line.replace("!", "");
-				}else if(line.indexOf('?') != -1) {
-					noPunctuation = line.replace("?", "");
-				}else if(line.indexOf("--") != -1) {
-					noPunctuation = line.replace("--", "");
-				}else if(line.indexOf('"') != -1) {
-					noPunctuation = line.replace("\"", "");
-				}else if(line.indexOf('\'') != -1) {
-					noPunctuation = line.replace("'", "");
-				}else if(line.indexOf('-') != -1) {
-					noPunctuation = line.replace("-", "");
-				}else {
-					noPunctuation = line;
-				}
-			System.out.println(b + " " + noPunctuation);
+			words[a] = words[a].replace('.', ' ');
+			words[a] = words[a].replace(',', ' ');
+			words[a] = words[a].replace(';', ' ');
+			words[a] = words[a].replace(':', ' ');
+			words[a] = words[a].replace('\'', ' ');
+			words[a] = words[a].replace('"', ' ');
+			words[a] = words[a].replace('!', ' ');
+			words[a] = words[a].replace('?', ' ');
+			words[a] = words[a].replace('-', ' ');
+			words[a] = words[a].trim().toLowerCase();
+		}
+		Arrays.sort(words);
+		for(int i = 1,  j = 1; i < words.length; i++) {
+			if(!(words[i].equals(words[i-1]))) {
+				System.out.println(j + " " + words[i]);
+				j++;
+			}
 		}
 	}
-	
+
 	public static String[] stringArray() throws IOException {
-		
+		BufferedReader f = new BufferedReader(new FileReader("Cabbages.txt"));
+		Scanner in = new Scanner(f);
+		int a = 0;
+		while(in.hasNext()) {
+			a++;
+			in.next();
+		}
+		in.close(); // Close the scanner so we can open a new one
+		BufferedReader g = new BufferedReader(new FileReader("Cabbages.txt"));
+		Scanner input = new Scanner(g);
+		String []words = new String[a];
+		for(int b = 0; input.hasNext(); b++) {
+			words[b] = input.next();
+		}
+		return words;
 	}
 
 }
