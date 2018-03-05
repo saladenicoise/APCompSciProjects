4bbcbf668cc3ac466d0133aa47e4e4602b3842dc
Jules Petit <jules.laptop@gmail.com>
2018-03-05 11:28:22 -0500

    Decks of Cards

---
+++ Lab15_JP/src/DeckTester.java
@@ -0,0 +1,18 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"A", "B", "C"};
+		String[] suits = {"Giraffes", "Lions"};
+		int[] values = {2, 1, 6};
+		System.out.println("Hi");
+		Deck deck = new Deck(ranks, suits, values);
+		System.out.println(deck.deal());
+	}
+}
