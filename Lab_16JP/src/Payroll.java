b754cbf31d57ac3a34f3883a6a2832dd01476cb1
Jules Petit <jules.laptop@gmail.com>
2018-03-27 08:41:32 -0400

    Payroll is done

--- Lab_16JP/src/Payroll.java
+++ Lab_16JP/src/Payroll.java
@@ -1,24 +1,64 @@
+import java.awt.Choice;
 import java.io.*;
 import java.util.*;
 
 public class Payroll {
 
-	private static ArrayList<Employee> payroll = new ArrayList<Employee>();
+	private static LinkedList<Employee> payroll = new LinkedList<Employee>();
 
+	/**
+	 * Main menu function
+	 * @param args
+	 * @throws notValidEmployeeTypeException not a valid employee type
+	 */
 	public static void main(String[] args) throws notValidEmployeeTypeException {
-		addEmployee("hourly");
-		//for(Employee e : payroll) {
-		//	System.out.println(e.toString());
-		//}
-		deleteEmployee("hourly");
-		if(payroll.size() > 0) {
-			for(Employee e : payroll) {
-				System.out.println(e.toString());
+		Scanner menuChoice = new Scanner(System.in);
+		int choice = 0;
+		while(choice != 6) {
+			System.out.println();
+			System.out.println("Welcome to payroll system v 1.0");
+			System.out.println("-------------------------------");
+			System.out.println("1. Display Payroll Information");
+			System.out.println("2. Add an Employee");
+			System.out.println("3. Remove an Employee");
+			System.out.println("4. Update hourly worker pay");
+			System.out.println("5. Calculate total payroll");
+			System.out.println("6. Quit");
+			System.out.println("-------------------------------");
+			System.out.print("Choice: ");
+			choice = menuChoice.nextInt();
+			switch(choice) {
+			case 1:
+				displayPayrollInfo();
+				break;
+			case 2:
+				Scanner employeeType = new Scanner(System.in);
+				System.out.println("Which kind of employee would you like to add(hourly/salaried): ");
+				String employeeTyp = employeeType.nextLine();
+				addEmployee(employeeTyp);
+				break;
+			case 3:
+				deleteEmployee();
+				break;
+			case 4:
+				updateEmployeeHours();
+				break;
+			case 5:
+				calculateTotalPayroll();
+				break;
+			default :
+				break;
 			}
-		}else {
-			System.out.println("Empty Thingy");
 		}
+		System.out.println("Goodbye!");
+		System.exit(0);
 	}
+	
+	/**
+	 * Adds an employee
+	 * @param employeeType hourly or salaried
+	 * @throws notValidEmployeeTypeException not a valid employee type
+	 */
 
 	public static void addEmployee(String employeeType) throws notValidEmployeeTypeException{
 		employeeType.toLowerCase();
@@ -50,29 +90,67 @@ public class Payroll {
 		}
 	}
 
-	public static void deleteEmployee(String employeeType) throws notValidEmployeeTypeException {
+	
+	/**
+	 * Deletes an employee given the id
+	 */
+	public static void deleteEmployee(){
 		Scanner userInput = new Scanner(System.in);
-		Scanner input = new Scanner(System.in);
-		if(employeeType.equals("hourly")) {
-			System.out.println("Employee Id: ");
-			String id = userInput.nextLine();
-			for(Iterator<Employee> e) {
-				if(e.getEmployeeId().equals(id)) {
-					payroll.remove(e);
-				}
+		System.out.println("Employee Id: ");
+		String id = userInput.nextLine();
+		for(Iterator<Employee> e = payroll.iterator(); e.hasNext();) {
+			Employee currEmployee = e.next();
+			if(currEmployee.getEmployeeId().equals(id)) {
+				payroll.remove(currEmployee);
 			}
-		}else if(employeeType.equals("salaried")) {
-			System.out.println("Name of employee: ");
-			String name = userInput.nextLine();
-			System.out.println("Employee ID: ");
-			String id = userInput.nextLine();
-			System.out.println("Current Rate of Pay: ");
-			double currRate = input.nextDouble();
-			SalariedEmployee employee = new SalariedEmployee(name, id, currRate);
-			payroll.remove(employee);
-		}else {
-			throw new notValidEmployeeTypeException("Incorect Employee Type");
 		}
 	}
 
+	/**
+	 * Update hourly employee hours
+	 */
+	public static void updateEmployeeHours() {
+		Scanner userInput = new Scanner(System.in);
+		Scanner userInput2 = new Scanner(System.in);
+		int counter = 0;
+		System.out.println("Employee Id: ");
+		String id = userInput.nextLine();
+		for(Iterator<Employee> e = payroll.iterator(); e.hasNext();) {
+			Employee currEmployee = e.next();
+			counter++;
+			if(currEmployee.getEmployeeId().equals(id)) {
+				String name = currEmployee.getEmployeeName();
+				System.out.println("How many hours has employee \"" + name + "\" worked: ");
+				double workHours = userInput2.nextDouble();
+				HourlyEmployee employee = new HourlyEmployee(name, id, currEmployee.getPay());
+				employee.setWorkHours(workHours);
+				payroll.set(counter, employee);
+			}
+		}
+	}
+
+	/**
+	 * Display Payroll
+	 */
+	public static void displayPayrollInfo() {
+		if(payroll.size() > 0) {
+			for(Employee e : payroll) {
+				System.out.println(e.toString());
+			}
+		}else {
+			System.out.println("No employees present");
+		}
+	}
+
+	/**
+	 * Calc total payroll
+	 * @return total
+	 */
+	public static double calculateTotalPayroll() {
+		double total = 0;
+		for(Employee e : payroll) {
+			total += e.paycheck();
+		}
+		return total;
+	}
 }
